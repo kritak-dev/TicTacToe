4643ee85f93132df177af611f0f7733f25fdcea7
kritak-dev <kritak.singh.che15@itbhu.ac.in>
2021-06-05 13:25:28 +0530

    Adding functionality for playing the game and deciding the result.

--- script.js
+++ script.js
@@ -16,44 +16,111 @@ const gameBoard = (() => {
         board = board.map(elem => elem = "");
     };
 
-    return { setField, getField, resetBoard, board };
+    return { setField, getField, resetBoard };
 })();
 
 // setting the Player factory function
-const Player = (sign) => {
-    const getSign = () => {
-        return sign;
-    };
+const Player = (name, sign) => {
+    const getSign = () => sign;
+    const getName = () => name;
 
-    return { getSign };
+    return { getSign, getName };
 };
 
-// adding the display controller
+// adding the display controller to upgrade, reset and 
 const displayController = (() => {
     const fields = document.querySelectorAll('.field');
     const resetButton = document.querySelector('.reset-button');
 
-    const upgradeBoard = () => {
-        for (let i = 0; i < fields.length; i++) {
+    const updateBoard = () => {
+        for (let i = 0; i < fields.length; i++)
             fields[i].textContent = gameBoard.getField(i);
-        }
     };
 
-    const reset = () => {
+    const resetBoard = () => {
         gameBoard.resetBoard();
-        upgradeBoard();
+        updateBoard();
     };
 
     fields.forEach(field => {
         field.addEventListener('click', function(evt) {
             let index = Number(evt.target.dataset.index);
-            gameBoard.setField(index, 'X');
-            upgradeBoard();
+            gameController.playGame(index);
+            if (gameController.isOver()) {
+                if (gameController.isGameDraw())
+                    console.log('Game Draw');
+                else
+                    console.log(`${gameController.getPlayer().getName()} is winner`);
+
+                gameController.resetBoard();
+            }
         });
     });
 
-    resetButton.addEventListener('click', (evt) => {
-        gameBoard.resetBoard();
-        upgradeBoard();
-    });
+    resetButton.addEventListener('click', resetBoard);
+
+    return { updateBoard, resetBoard }
+})();
+
+const gameController = (() => {
+    player1 = Player('PlayerX', 'X');
+    player2 = Player('PlayerO', 'O');
+    let player = player1;
+    let isDraw = false, gameOver = false;
+
+    const playGame = (index) => {
+        gameBoard.setField(index, player.getSign());
+        displayController.updateBoard();
+        if (checkWinner(index)) {
+            gameOver = true;
+            return player;
+        }
+
+        if (checkDraw()) {
+            gameOver = true;
+            isDraw = true;
+            return;
+        }
+
+        player = player === player1 ? player2 : player1;
+    };
+
+    const checkDraw = () => {
+        for (let i = 0; i < 9; i++) {
+            if (gameBoard.getField(i) === "")
+                return false;
+        }
+
+        return true;
+    }
+
+    const checkWinner = (index) => {
+        const winCondition = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        return winCondition
+        .filter(combination => combination.includes(index))
+        .some(possibleCombination => possibleCombination.every(ind => gameBoard.getField(ind) === player.getSign()));
+    }
+
+    const isOver = () => gameOver;
+    const isGameDraw = () => isDraw;
+    const getPlayer = () => player;
+
+    const resetBoard = () => {
+        player = player1;
+        gameOver = false;
+        isDraw = false;
+        displayController.resetBoard();
+    }
+    
+    return { playGame, isOver, isGameDraw, getPlayer, resetBoard }
 })();
